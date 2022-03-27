eed7f2ebe88cf26d59991171d3b5352307674085
Marcel Wiget <m.wiget@f5.com>
2022-03-27 06:04:51 +0000

    test LB script via curl

--- send-curl-request.sh
+++ send-curl-request.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ip=$(cd azure-workload-1 && terraform output | grep pip | cut -d\" -f2)
-echo -n "$ip ... "
-ssh -o StrictHostKeyChecking=no azureuser@$ip curl -H host:workload.azure1.example.internal 100.64.17.5/txt
+cmd="curl -H host:workload.azure1.example.internal 100.64.17.5/txt"
+echo "$cmd ..."
+ssh -o StrictHostKeyChecking=no azureuser@$ip $cmd
